8e1b07fab1f11ce6548f784ac3dd7e75ccd4ec15
Biruk <Biruk Saleamlak>
2023-07-17 14:39:00 -0700

    python formatters

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    print("{0:02}".format(i), end=", ")
-print()
+    if i == 99:
+        print("{:02}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
